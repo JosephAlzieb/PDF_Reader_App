c16e6ee279f187931b0366107cf3149c50b8e239
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-04-29 22:21:56 +0200

    add the list of documents to the homepage

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:pdf_reader_app/document.dart';
+import 'package:pdf_reader_app/screen_reader.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
-  TextStyle textStyle(double fontSize) {
-    return TextStyle(color: Colors.white, fontSize: fontSize);
+  TextStyle textStyle(Color color, double fontSize) {
+    return TextStyle(color: color, fontSize: fontSize);
   }
 
   @override
@@ -15,7 +17,7 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.deepOrangeAccent,
         title: Text(
           "PDF Reader",
-          style: textStyle(30),
+          style: textStyle(Colors.white, 30),
         ),
         leading: const Icon(Icons.menu_outlined),
       ),
@@ -24,7 +26,7 @@ class HomePage extends StatelessWidget {
           padding: const EdgeInsets.all(10),
           child: Column(
             children: [
-              Text(
+              const Text(
                 "your Documents",
                 style: TextStyle(
                   fontSize: 30,
@@ -32,6 +34,34 @@ class HomePage extends StatelessWidget {
                   color: Colors.black45,
                 ),
               ),
+              Column(
+                children: Document.docuList
+                    .map((docu) => ListTile(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        ScreenReader(document: docu)));
+                          },
+                          leading: const Icon(
+                            Icons.picture_as_pdf,
+                            color: Colors.deepOrange,
+                            size: 45,
+                          ),
+                          subtitle: Text(
+                            "num. of pages: ${docu.pageNum}",
+                            style: textStyle(Colors.black45, 17),
+                          ),
+                          trailing: Text(docu.date),
+                          title: Text(
+                            docu.title,
+                            style: textStyle(Colors.black54, 25),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ))
+                    .toList(),
+              )
             ],
           ),
         ),
